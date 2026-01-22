ed6747f2751a013a7b42e52cce8cd96d1a4235b7
kamilbogoss <cinark@D350-S18>
2026-01-22 09:00:26 +0100

    creation classe devise

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,24 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        private int id;
+        private string nomdevise;
+        private double taux;
+
+        public Devise()
+        {
+        }
+
+        public Devise(int id, string nomdevise, double taux)
+        {
+            this.Id = id;
+            this.Nomdevise = nomdevise;
+            this.Taux = taux;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nomdevise { get => nomdevise; set => nomdevise = value; }
+        public double Taux { get => taux; set => taux = value; }
+    }
+}
